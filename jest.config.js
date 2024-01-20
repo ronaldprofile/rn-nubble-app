8ea63aaaaad5084b507c75763c6022649220f4c0
Ronald Tomaz <ronaldtmprofile@gmail.com>
2024-01-20 11:58:20 -0300

    setup RNTL

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'react-native',
   collectCoverageFrom: ['src/{components,utils}/**/*.{js,jsx,ts,tsx}'],
-  coveragePathIgnorePatterns: ['/node_modules/', 'index']
+  coveragePathIgnorePatterns: ['/node_modules/', 'index'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect']
 }
